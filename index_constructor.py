9b3147b9c29aa2cfa7671f8a3d2660a1fec3056f
Rebecca Park <sbvts177@gmail.com>
2024-02-08 17:20:03 -0800

    added files for basic set up of code

---
+++ index_constructor.py
@@ -0,0 +1,4 @@
+#creating inverted index
+
+#use lmxl to parse html and extract text + handle broken html
+#use nltk for tokenization and lemmatization
---
+++ main.py
@@ -0,0 +1,8 @@
+#running the actual code
+
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
