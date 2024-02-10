68cb4aee0cbbf2ed649f258a70d57b2e0ddddb7b
Rebecca Park <sbvts177@gmail.com>
2024-02-09 16:05:28 -0800

    skipping none html files

--- index_constructor.py
+++ index_constructor.py
@@ -3,6 +3,7 @@
 #use lmxl to parse html and extract text + handle broken html
 #use nltk for tokenization and lemmatization
 
+import json
 from lxml import html
 import os.path
 
@@ -23,13 +24,25 @@ class index_constructor():
         pass
 
     #parse given html file and extract the text content
-    def parse_and_extract_text(self, html_file):
-        f = open(html_file, "r")
-        html_page = html.fromstring(f.read())
-        f.close()
-        return html_page.text_content()
+    def parse_and_extract_text(self, html_file_location):
 
-    #maybe should be moved to main.py??
+        #skip non html file
+        try:
+            #get file
+            f = open(html_file_location, "r")
+
+            page = f.read()
+            f.close()
+
+            #parse html
+            html_page = html.fromstring(page)
+            
+            text_content = html_page.text_content()
+        except:
+            return ""
+        return text_content
+
+    #maybe should be moved to main.py???
     #go through bookkeeping.json and extract text from each html file for now
     def read_files(self):
         f = open(os.path.join(self.webpages_path,"bookkeeping.json"))
@@ -37,7 +50,8 @@ class index_constructor():
         f.close()
         for file_location in file_dict.keys():
             folder,file_name = file_location.split("/")
-            html_text = self.parse_html(os.path.join(self.webpages_path,folder,file_name))
+            html_text = self.parse_and_extract_text(os.path.join(self.webpages_path,folder,file_name))
+            #do tokenization and lemmatization on text next
 
         
 
